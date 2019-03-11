cfb3b06651b341a8705aac1715ab28a5eed373d7
Imran Settuba <ulti_mate@Imrans-MacBook-Pro.local>
2019-03-11 16:43:29 +0100

    table suggestion

---
+++ api.sql
@@ -0,0 +1,11 @@
+create table locations(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    category VARCHAR(20) NOT NULL,
+    name VARCHAR(60) NOT NULL,
+    address VARCHAR(140) NOT NULL,
+    room VARCHAR(140),
+    transport_station VARCHAR(150), 
+    opening_hours VARCHAR(300), 
+    infos VARCHAR(500),
+    url VARCHAR(300) NOT NULL
+);;
